1e1dbe8b75083e9fdd81ae0656b3cadced06fe67
kenanhaskic <kenan.haskic@edu.fit.ba>
2020-01-23 13:32:45 +0100

    landing

---
+++ index.js
@@ -0,0 +1,13 @@
+
+    function sum()
+    {
+        var a=document.getElementById('input').value;
+        var b=document.getElementById('input2').value;
+        var result=0;
+        result=parseInt(a)+parseInt(b);
+        document.getElementById('result').innerHTML=result;
+    
+    }
+
+
+    
